63f578a1e4605f4221fe8f965e2087459402399d
Ereur <amoussaouianas@gmail.com>
2022-06-07 20:12:49 +0100

    losing data in threads

--- main.c
+++ main.c
@@ -7,16 +7,17 @@ void	print_action(t_philo *philos)
 {
 	// pthread_mutex_lock(&(philos->writing));
 	printf("im philo %d im a live\n", philos->id);
+	// printf("time to die %d\n",philos->tasks->time_to_die);
 	// pthread_mutex_unlock(&(philos->writing));
 }
 
-void *routin(void *philos)
+void *routin(t_philo *philos)
 {
-	t_philo	*philo;
-
-	philo = (t_philo *)philos;
+	// t_philo	*philo = (t_philo *)philos;
 	// printf("%d \n", philo->id);
-	print_action(philo);
+	// printf("hello\n");
+	printf("time to die %d\n",philos->tasks->time_to_die);
+	print_action(philos);
 	return	(NULL);
 }
 
@@ -25,7 +26,7 @@ void ft_init_mutex(t_philos *philos)
 	int	i;
 
 	i =  0;
-	while (i < philos->philo_tasks.nb_of_philos)
+	while (i < philos->nb_of_philos)
 	{	
 		pthread_mutex_init(&(philos->forks[i]), NULL);
 		i++;
@@ -34,31 +35,50 @@ void ft_init_mutex(t_philos *philos)
 
 void ft_init_philos(t_philos *philos, int ac, char **argv)
 {
-	philos->philo_tasks.nb_of_philos = ft_atoi(argv[1]);
-	philos->philo_tasks.time_to_die = ft_atoi(argv[2]);
-	philos->philo_tasks.time_to_eat = ft_atoi(argv[3]);
-	philos->philo_tasks.time_to_sleep = ft_atoi(argv[4]);
+	int		i;
+	t_tasks	rules;
+	i = 0;
+	rules.nb_of_philos = ft_atoi(argv[1]);
+	philos->nb_of_philos = rules.nb_of_philos;
+	rules.time_to_die = ft_atoi(argv[2]);
+	rules.time_to_eat = ft_atoi(argv[3]);
+	rules.time_to_sleep = ft_atoi(argv[4]);
 	if (ac == 6)
-		philos->philo_tasks.number_of_eats = ft_atoi(argv[5]);
+		rules.number_of_eats = ft_atoi(argv[5]);
 	else
-		philos->philo_tasks.number_of_eats = -1;
-	ft_init_mutex(philos);
+		rules.number_of_eats = -1;
+	philos->philo = malloc(sizeof(t_philo *) * (rules.nb_of_philos + 1));
+	i = 0;
+	while(i < rules.nb_of_philos)
+		philos->philo[i++] = malloc(sizeof(t_philo));
+	i = 0;
+	while (i < rules.nb_of_philos)
+		philos->philo[i++]->tasks = &rules;
 	
+	// i = 0;
+	// while (i < rules.nb_of_philos)
+	// {
+	// 	printf("%d\n", philos->philo[i++].tasks->time_to_die);
+	// }
+	// exit(1);
+	ft_init_mutex(philos);
 }
-
-
 
 void ft_lunch_philos(t_philos *philos)
 {
+	t_philo *philo;
+
+	philo = philos->philo[0];
 	int i;
 
 	i = 0;
 
-	while (i < philos->philo_tasks.nb_of_philos)
+	while (i < philos->nb_of_philos)
 	{
-		philos->philo[i].id = i + 1;
-		// printf("%d\n", philos->philo[i].id);
-		pthread_create(&(philos->philo[i].philo), NULL, routin, &philos->philo[i]);
+		philos->philo[i]->id = i + 1;
+		// printf("%d\n", philos->philo[i]->id);
+		routin(philo);
+		// pthread_create(&(philos->philo[i]->philo), NULL, routin, &(philos->philo[i]));
 		i++;
 		// usleep(1000);
 	}
@@ -69,9 +89,9 @@ void ft_wait_philos(t_philos *philos)
 {
 	int i;
 	i = 0;
-	while (i < philos->philo_tasks.nb_of_philos)
+	while (i < philos->nb_of_philos)
 	{
-		pthread_join(philos->philo[i].philo, NULL);
+		pthread_join(philos->philo[i]->philo, NULL);
 		i++;
 	}
 }
@@ -81,6 +101,11 @@ int main(int ac, char **argv)
 	t_philos philos;
 
 	ft_init_philos(&philos, ac, argv);
+	// int i = 0;
+	// while (i < philos.nb_of_philos)
+	// {
+	// 	printf("%d\n", philos.philo[i++].tasks->time_to_die);
+	// }
 	ft_lunch_philos(&philos);
 	ft_wait_philos(&philos);
 }
